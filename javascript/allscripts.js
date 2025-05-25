daed40c25fc5e67efbef5e3df831efc3bc2a77c9
soki <qisyauma@gmail.com>
2025-05-25 23:32:19 +0700

    nig

--- javascript/allscripts.js
+++ javascript/allscripts.js
@@ -1,13 +1,19 @@
-let meuList = document.getElementById("menuList")
+let meuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 
-function toggleMenu(){
-    if(menuList.style.maxHeight == "0px")
-    {
-        menuList.style.maxHeight = "50vh";
-    }
-    else{
-        menuList.style.maxHeight = "0px";
-    }
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "50vh";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
 }
 
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar-container");
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-container-scrolled");
+  } else {
+    navbar.classList.remove("navbar-container-scrolled");
+  }
+});
