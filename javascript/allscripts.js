505862af244707d9e490c53446204a3b0ced6206
soki <qisyauma@gmail.com>
2025-06-03 00:41:32 +0700

    hiii

--- javascript/allscripts.js
+++ javascript/allscripts.js
@@ -17,3 +17,21 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-container-scrolled");
   }
 });
+
+const parentContainer = document.querySelector('.pp-container-2-subcontainer-2');
+
+parentContainer.addEventListener('click', event=>{
+
+  const current = event.target;
+
+  const isReadMoreBtn = current.className.includes('read-more-btn');
+
+  if(!isReadMoreBtn) return;
+
+  const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+  currentText.classList.toggle('read-more-text--show');
+
+  current.textContent = current.textContent.includes('Show More') ?
+  "Show Less..." : "Show More..."
+})
